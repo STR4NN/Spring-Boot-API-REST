544869345aae8ec48198c1f38b088c4234a1f16e
Gustavo Lopes <gustavo.souza0911@gmail.com>
2025-03-13 12:13:58 -0300

    understanding the annotations of Spring Bott

---
+++ src/main/java/apirest/apiresttest/ApiresttestApplication.java
@@ -0,0 +1,14 @@
+package apirest.apiresttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiresttestApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiresttestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apirest/apiresttest/ApiresttestApplicationTests.java
@@ -0,0 +1,13 @@
+package apirest.apiresttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiresttestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
